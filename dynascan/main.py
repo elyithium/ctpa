e2cd468c15a3a57fffcbde45ff86d99e510be6fd
Ali Mohammad Jobaer <103154804+jobaer4810@users.noreply.github.com>
2024-09-17 22:10:29 +1000

    Reconnaissance added to the scanner engine

--- dynascan/main.py
+++ dynascan/main.py
@@ -1,4 +1,4 @@
-# main.py
+
 from scanner_engine.vulnerability_scanner import VulnerabilityScanner
 
 if __name__ == "__main__":
@@ -8,15 +8,16 @@ if __name__ == "__main__":
         "WebGoat/Auth/login": {"username": "test", "password": "test"},
         "WebGoat/SensitiveData": {},
         "WebGoat/AccessControl/attack": {},
-        "WebGoat/Deserialization/attack": {"data": "test"}
+        "WebGoat/Deserialization/attack": {"data": "test"},
+        "WebGoat/login": {}
     }
 
     base_url = "http://127.0.0.1:8080/"
+    target_ip = "127.0.0.1"
 
-    target_ip = "127.0.0.1"  
+    host_info_url = "http://127.0.0.1:8080/WebGoat/login"
 
-    scanner = VulnerabilityScanner(base_url, endpoints, target_ip)
-    
+    scanner = VulnerabilityScanner(base_url, endpoints, target_ip, host_info_url)
+
     scanner.run_scans()
-
     scanner.generate_report()
